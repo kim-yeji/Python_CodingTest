6d4396cc54a21a61cb3af40d76ce4d9cb88616ad
yezzi <37108655+kim-yeji@users.noreply.github.com>
2020-07-25 22:56:34 +0900

    200725 sat
    뮤직비디오(결정알고리즘)
    
    지니레코드에서는 불세출의 가수 조영필의 라이브 동영상을 DVD로 만들어 판매하려 한다.
    DVD에는 총 N개의 곡이 들어가는데, DVD에 녹화할 때에는 라이브에서의 순서가 그대로 유지
    되어야 한다. 순서가 바뀌는 것을 우리의 가수 조영필씨가 매우 싫어한다. 즉, 1번 노래와 5번
    노래를 같은 DVD에 녹화하기 위해서는 1번과 5번 사이의 모든 노래도 같은 DVD에 녹화해야
    한다. 또한 한 노래를 쪼개서 두 개의 DVD에 녹화하면 안된다.
    지니레코드 입장에서는 이 DVD가 팔릴 것인지 확신할 수 없기 때문에 이 사업에 낭비되는
    DVD를 가급적 줄이려고 한다. 고민 끝에 지니레코드는 M개의 DVD에 모든 동영상을 녹화하기
    로 하였다. 이 때 DVD의 크기(녹화 가능한 길이)를 최소로 하려고 한다. 그리고 M개의 DVD는
    모두 같은 크기여야 제조원가가 적게 들기 때문에 꼭 같은 크기로 해야 한다.
    
    설명 : 3개의 DVD용량이 17분짜리이면 (1, 2, 3, 4, 5) (6, 7), (8, 9) 이렇게 3개의 DVD로 녹음을 할
    수 있다. 17분 용량보다 작은 용량으로는 3개의 DVD에 모든 영상을 녹화할 수 없다.

---
+++ MusicVideo.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin=open("input.txt","rt")
+
+def Count(capacity):
+    cnt=1 # DVD 한 장은 꼭 필요함
+    sum=0 # 음악 길이를 더해볼 것
+    for x in music:
+        if sum+x > capacity: # x까지 더한 값은 capacity를 초과하므로 x는 넣을 수 없음
+            cnt+=1 # 새로운 DVD 필요함
+            sum=x # x부터 다시 새로 저장한다.
+        else:
+            sum+=x
+    return cnt
+
+n,m=map(int,input().split())
+music=list(map(int,input().split()))
+
+lt=1
+rt=sum(music)
+answer=0
+
+while lt<=rt:
+    mid=(lt+rt)//2
+    if Count(mid)<=m : # 필요한 DVD 개수가 넘어옴
+        answer=mid
+        rt=mid-1
+    else:
+        lt=mid+1
+print(answer)
+
+
+
+
