eb97105558a68f4d6e986d2fa37a06dc064283ef
yezzi <37108655+kim-yeji@users.noreply.github.com>
2020-07-28 12:11:11 +0900

    200728 tue Deque ver.
    Deque 버전
    침몰하는 타이타닉(그리디)
    
    유럽에서 가장 유명했던 유람선 타이타닉이 침몰하고 있습니다. 유람선에는 N명의 승객이 타고
    있습니다. 구명보트를 타고 탈출해야 하는데 타이타닉에 있는 구명보트는 2명 이하로만 탈 수 있
    으며, 보트 한 개에 탈 수 있는 총 무게도 M kg 이하로 제한되어 있습니다.
    N명의 승객 몸무게가 주어졌을 때 승객 모두가 탈출하기 위한 구명보트의 최소개수를 출력하는
    프로그램을 작성하세요.

---
+++ Sinking_Titanic_Deque.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+sys.stdin=open("input.txt","rt")
+
+n, limit = map(int,input().split())
+people=list(map(int,input().split()))
+
+people=deque(p)
+
+cnt=0
+while people: #deque 자료구조가 다 비워지면 멈춘다.
+    if len(people)==1: # 사람이 혼자 남을 때, 뒤에 논리 모순을 방지하기 위해!
+        cnt+=1
+    if people[0]+people[-1]>limit: #limit 넘어가므로 가장 무거운 사람은 혼자 타야함
+        p.pop()
+        cnt+=1
+    else:
+        people.popleft(0)
+        people.pop()
+        cnt+=1
+
+print(cnt)
