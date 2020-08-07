afe8eef8e7eb705d34a14c550e11c989e39e8cbb
yezzi <37108655+kim-yeji@users.noreply.github.com>
2020-08-07 11:28:52 +0900

    200807 fri
    이진트리 순회(깊이우선탐색)

---
+++ Binary_Tree_Circuit.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin=open("input.txt","rt")
+
+def DFS(v):
+    if v>7:
+        return
+    else:
+        #print(v, end='') #전위순회
+        DFS(v*2)
+        #print(v, end='') #중위순회
+        DFS(v*2+1)
+        print(v, end='') #후위순회(병합정렬)
+
+if __name__=="__main__":
+    DFS(1)
